d6e6393d426de17c46c8c5ef6727d51da9cade05
YAKOU Naima <66495043+naima1994@users.noreply.github.com>
2021-05-16 14:27:20 +0100

    Update webblocker.py

--- webblocker.py
+++ webblocker.py
@@ -1,4 +1,4 @@
-#this app allow you to block access to websites of your choices in anytime you want. it can be used as a parental control mappings
+#this app allows you to block access to websites of your choices in anytime you want. it can be used as a parental control apps
 #it blocks the websites by redirecting the links to those websites to the localhost ip by adding entries to the windows host file
 # this app is designed to be scheduled on every time windows lunchs and will stay running in the background
 #save the script as .pyw to be run in the background
@@ -16,7 +16,7 @@ while True :
     # the program check every 60 sec if the actual time is between working hours , if it's true
     # the program adds the items on the list sitelist to the host file with the ip of localhost so they will be redirected
     if dt(dt.now().year,dt.now().month,dt.now().day,working_hours[0]) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,working_hours[1]):
-        print('working hours') # used only for testing
+        #print('working hours') # used only for testing
         with open(host_temp,'r+') as file :
             content= file.read()
             for w in sites_list:
@@ -31,7 +31,7 @@ while True :
             content=file.readlines()
             file.seek(0)
             for line in content:
-                if not any(website in line for website in website_list):
+                if not any(website in line for website in sites_list):
                     file.write(line)
         file.truncate() #this line is used so the text will be writen only once
         # print("enjoy your free time")  # this line is used for testing
